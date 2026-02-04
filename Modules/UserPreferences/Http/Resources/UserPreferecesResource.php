3c9336468b65113356f562bc5c4c6f920ec9ecbc
diogogoncalves <dmgdark95@gmail.com>
2026-02-04 14:31:38 +0000

    feat(users): Add User Preferences Module

---
+++ Modules/UserPreferences/Http/Resources/UserPreferecesResource.php
@@ -0,0 +1,20 @@
+<?php
+namespace Modules\UserPreferences\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserPreferecesResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'lang'        => $this->lang,
+            'currency_id' => $this->currency_id,
+            'currency'    => $this->currency,
+        ];
+    }
+}
